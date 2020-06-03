53e5c290632241ed3f3bb30417543659a10c5cf3
BeLuckyDaf <beluckydaf@gmail.com>
2020-06-03 20:32:28 +0300

    Add services note.

--- services/services.go
+++ services/services.go
@@ -12,6 +12,15 @@ import (
 /* Server & Gameplay Services */
 /* ========================== */
 
+/**
+ * Services include bots and other active in-game entities as well, not only system events.
+ *
+ * They can be used to change the state of the game due to different events or simply to
+ * serve as a notifier and periodically send in-game messages. Basically, on every tick a service
+ * has an ability to change game state, broadcast messages to online players, write to the
+ * persistent storage, access the match context, see players messages that arrived since the last tick.
+ */
+
 // SBServiceInterface is used for different services which are called in MatchLoop
 type SBServiceInterface interface {
 	Init(config *core.SBConfig)
